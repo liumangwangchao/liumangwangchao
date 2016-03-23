817b19893de970a49c3c356072ab1cfea90ccd94
liumangwangchao <selang168@gmail.com>
2016-03-23 20:05:53 +0800

    commit messages

---
+++ ooxx/bo.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta charset="utf-8" />
+<title>sanjiplayer by selang168@gmail.com</title>
+</head>
+<body bgcolor="#000000" style="background-color:black; margin:0; padding:0;">
+<center>
+<script>
+document.write('<video src="http://mp4.123456mp4.com/<?php echo $_GET['id']; ?>" controls="controls" autoplay="autoplay" poster="http://ww3.sinaimg.cn/mw690/005GHnlCjw1f22ocdohobj30hs0a074r.jpg" width="100%" height="100%" type="video/mp4"></video>')
+</script>
+</center>
+</body>
+<div style="display:none;">
+<script>var lainframe;</script>
+
+</html>
---
+++ ooxx/index.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no">
+<title>《流氓王朝三级在线播放器》</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+</head>
+<div class="wrap">
+<div class="n_header">
+<div class="cont-banner"> 
+<a href="http://t.cn/RGe59QO" id="tudouWeixinID">小伙伴都在精彩分享最新地址：http://t.cn/RGe59QO</a></div>
+</div><div class="w98 mian_bo">
+<div class="so_list clearfix w98 v_txt">
+<iframe width="100%" height="240px" src="bo.php?id=<?php echo $_GET['id']; ?>" frameborder=0 border=0 marginwidth=0 marginheight=0 scrolling=no></iframe>
+<p align="center" class="STYLE2 STYLE1">手机推荐UC浏览器,电脑推荐360极速浏览器(兼容模式)</p>
+<div class="btn">
+<a href="javascript:void(0);" onClick="document.location.reload()" class="btn1">切换线路</a>
+<a href="http://t.cn/RGe59QO" class="btn2">电影首页</a></div>
+<div style="display: none">
+       <p> <img src="https://mmbiz.qlogo.cn/mmbiz/vKTEibEJx2FtGsibK3YLvSHl1SD3vdCBNneRkGE0Bvic4rpWvLWTDFNJZUtj70th9kQAMfVzvwV0YJaqvsqcPGCWw/0?wx_fmt=jpeg" /></p>
+      </div> 
+<p><img src="http://ww4.sinaimg.cn/mw690/005GHnlCjw1f23bzjd36uj30fb0a1q41.jpg" height="100%" width="100%" alt="公众号图片"></p>
+<p align="center"><script src="pplay.js" charset="gbk"></script></p>	
+</div>
+</body></html>
